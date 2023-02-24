a5706bd5ca19a6e8521ae5c049dd57c6c9b21fc0
Suganya-arun <100554621+Suganya-arun@users.noreply.github.com>
2023-02-24 17:05:45 +0530

    Update index.js
    Updated the index.js to change the prevent default position

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const form=document.querySelector("form");
 form.addEventListener("submit",verification);
 function verification(e)
 {
-    e.preventDefault();
     var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     // console.log(e.target[0].value.match(mailformat));
     if(!(e.target[0].value.match(mailformat)))
     {
+           e.preventDefault();
      e.target[0].style.borderColor="hsl(0, 93%, 68%)"; 
      e.target[0].style.borderWidth="2px";
      document.getElementById("error-icon").style.display="block";
