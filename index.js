e637e146bc3c4368e6082d4b9cd4135340c5c5f7
Suganya-arun <100554621+Suganya-arun@users.noreply.github.com>
2023-02-24 17:01:20 +0530

    The initial files of the assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const form=document.querySelector("form");
+form.addEventListener("submit",verification);
+function verification(e)
+{
+    e.preventDefault();
+    var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    // console.log(e.target[0].value.match(mailformat));
+    if(!(e.target[0].value.match(mailformat)))
+    {
+     e.target[0].style.borderColor="hsl(0, 93%, 68%)"; 
+     e.target[0].style.borderWidth="2px";
+     document.getElementById("error-icon").style.display="block";
+     document.querySelector("small").style.display="block";
+
+     console.log(e);
+    }
+    
+}
